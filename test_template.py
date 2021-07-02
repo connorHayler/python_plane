2c0aa17a652058b34b0e335092143a275eaa810a
Connor <connorhaylers@gmail.com>
2021-07-02 15:57:56 +0100

    imported pytest and Passenger

--- test_template.py
+++ test_template.py
@@ -1,11 +1,13 @@
 import unittest
 import json
+import pytest
+from passenger import Passenger
 
 class Testing(unittest.TestCase):
     passenger = Passenger("fName", "lName", "passportID")
     def test_passenger_create(self):
-        self.assertIsInstance(passenger, Passenger)
-        self.assertIsNotNone(passenger.f_name)
+        self.assertIsInstance(self.passenger, Passenger)
+        self.assertIsNotNone(self.passenger.f_name)
         with open("passenger_records.json", "r") as file:
             json_file = json.load(file)
             passenger_json = json_file["passenger"]
