8166e5d1b8aa78c3314d302d67da645f97229ef4
Connor <connorhaylers@gmail.com>
2021-07-02 17:04:12 +0100

    finished second unit test

--- test_template.py
+++ test_template.py
@@ -5,6 +5,7 @@ from passenger import Passenger
 
 class Testing(unittest.TestCase):
     passenger = Passenger("fName", "lName", "passportID")
+    flight = Flight("origin", "destination", "vehicle", "departure time")
     passenger.add()
     def test_passenger_create(self):
         self.assertIsInstance(self.passenger, Passenger)
@@ -13,7 +14,22 @@ class Testing(unittest.TestCase):
             json_file = json.load(file)
             passenger_json = json_file["passenger"]
             for person in passenger_json:
-                if person["passportNumber"] == "passportID":
-                    id = person["passportNumber"]
-            self.assertEquals(id, "passportID")
+                if person["passport"] == "passportID":
+                    id = person["fName"]
+                    break
+            self.assertEquals(id, "fName")
+
+    def test_flight_trip(self):
+        self.assertIsInstance(self.flight, Flight)
+        with open("flight_records.json") as file:
+            json_file = json.load(file)
+            flight_json = json_file["flight"]
+            for flight in flight_json:
+                if flight["flightID"] == "FlightID":
+                    destination = flight["destination"]
+                    break
+            self.assertEquals(destination, "destination")
+
+
+
 
